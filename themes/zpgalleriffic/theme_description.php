6fdcc8a9bd4a18bad54083a0bab19c354b65453e
acrylian <wartgart@gmx.net>
2014-01-24 19:48:36 +0100

    Since gjr abandoned his older themes we store them here for achival purposes.

---
+++ themes/zpgalleriffic/theme_description.php
@@ -0,0 +1,8 @@
+<?php
+// Zenphoto theme definition file
+$theme_description['name'] = 'zpGalleriffic';
+$theme_description['author'] = 'gjr';
+$theme_description['version'] = '1.4.4.3';
+$theme_description['date'] = '3/6/2013';
+$theme_description['desc'] = 'ZenPhoto/ZenPage Theme.';
+?>
---
+++ themes/zpskeleton/theme_description.php
@@ -0,0 +1,8 @@
+<?php
+// Zenphoto theme definition file
+$theme_description['name'] = 'zpskeleton';
+$theme_description['author'] = 'gjr';
+$theme_description['version'] = '1.4.3';
+$theme_description['date'] = '7/3/12';
+$theme_description['desc'] = 'A ZenPhoto/ZenPage responsive theme ready for desktop to mobile device viewing.';
+?>
