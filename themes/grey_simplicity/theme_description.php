2612ae1036d7453ff0eb823a9b6c96be80162a03
fretzl <f.sondaar@kpnmail.nl>
2014-05-04 16:27:27 +0200

    Theme grey-simplicity. Add HighSlide script, theme options and theme_description.

---
+++ themes/grey_simplicity/theme_description.php
@@ -0,0 +1,8 @@
+<?php
+// Zenphoto theme definition file
+$theme_description['name'] = 'grey-simplicity';
+$theme_description['author'] = '<a href="http://gilles.dodinet.fr" target="_blank">Gilles Dodinet (gdodinet)</a>';
+$theme_description['version'] = true;
+$theme_description['date'] = true;
+$theme_description['desc'] = 'Derived from Highslide theme, inspired by M9 theme, use Hermik\'s icons';
+?>
