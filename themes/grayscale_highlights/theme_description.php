099084144263938ad5510157040199b376c6d196
fretzl <f.sondaar@kpnmail.nl>
2014-05-15 18:31:11 +0200

    Incorrect theme name

--- themes/grayscale_highlights/theme_description.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Zenphoto theme definition file
-$theme_description['name'] = 'Gray Highlights';
-$theme_description['author'] = '<a href="http://www.thewholelifetolearn.eu" target="_blank">The Whole Life To Learn</a>';
-$theme_description['version'] = '1.0.0';
-$theme_description['date'] = '17/10/2011';
-$theme_description['desc'] = 'A theme with grayscales color and a touch of braun and green colors. The theme is based on a modified version of Grid960. It uses CSS3 and no Javascript is used for the theme itself.';
-?>
