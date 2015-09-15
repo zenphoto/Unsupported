1f8fca3aec7f07ee94c71fad378e3fe29c78e12f
fretzl <f.sondaar@kpnmail.nl>
2015-09-15 20:09:20 +0200

    Some typo's

--- themes/zpfocus/album.php
+++ themes/zpfocus/album.php
@@ -1,34 +1,34 @@
 <?php include("inc-header.php"); ?>
 <?php include("inc-sidebar.php"); ?>
-		
+
 <div class="right">
 	<?php if ($zpfocus_social) include ("inc-social.php"); ?>
-	<h1 id="tagline"><?php printParentBreadcrumb("", " / ", " / "); printAlbumTitle(); ?></h1>		
-	
+	<h1 id="tagline"><?php printParentBreadcrumb("", " / ", " / "); printAlbumTitle(); ?></h1>
+
 	<?php if ($zpfocus_logotype) { ?>
 	<a style="display:block;" href="<?php echo getGalleryIndexURL(); ?>"><img src="<?php echo $_zp_themeroot; ?>/images/<?php echo $zpfocus_logofile; ?>" alt="<?php echo getBareGalleryTitle(); ?>" /></a>
 	<?php } else { ?>
 	<h2 id="logo"><a href="<?php echo getGalleryIndexURL(); ?>"><?php echo getBareGalleryTitle(); ?></a></h2>
 	<?php } ?>
-	
+
 	<div class="album-details">
 		<?php printAlbumDate('','',null,true); ?>
 		<div class="album-tags"><?php printTags( 'links',gettext('| Tags:  '),'taglist',', ',true,'',true  ); ?></div>
 	</div>
 	<p class="description"><?php printAlbumDesc( true,'',gettext('Edit Description...') ); ?></p>
-	
+
 	<?php if (isAlbumPage()) { ?>
 	<div class="subalbum-wrap">
 		<h4 class="blockhead"><span><?php echo gettext('SubAlbums in '); echo getBareAlbumTitle(); ?> (<?php echo getNumAlbums(); ?>)</span></h4>
 		<ul>
-			<?php $x = 1; 
+			<?php $x = 1;
 				while (next_album()):
 				if( $odd = $x%2 ) {
 					$css = 'goleft';
 					} else {
 					$css = 'goright';
 				} ?>
-			<li class="<?php echo $css; ?>">	
+			<li class="<?php echo $css; ?>">
 				<h4><a href="<?php echo htmlspecialchars(getAlbumURL());?>" title="<?php echo gettext('View SubAlbum:'); ?> <?php echo getBareAlbumTitle();?>"><?php echo truncate_string(getBareAlbumTitle(), 20, '...'); ?></a></h4>
 				<a class="thumb" href="<?php echo htmlspecialchars(getAlbumURL());?>" title="<?php echo gettext('View SubAlbum:'); ?> <?php echo getBareAlbumTitle();?>">
 					<?php if (isLandscape()) {
@@ -36,33 +36,33 @@
 						} else {
 						printCustomAlbumThumbImage(getBareAlbumTitle(), null, 120, 160, 120, 160);
 					} ?>
-				</a>					
+				</a>
 				<span class="front-date"><?php printAlbumDate(); ?></span>
 				<p class="front-desc">
 					<?php echo truncate_string(getAlbumDesc(), 175); ?>
 					<a href="<?php echo htmlspecialchars(getAlbumURL());?>" title="<?php echo gettext('View SubAlbum:'); ?> <?php echo getBareAlbumTitle();?>">&raquo;</a>
 				</p>
 			</li>
-			<?php $x = $x+1; 
-			endwhile; ?>			
+			<?php $x = $x+1;
+			endwhile; ?>
 		</ul>
 	</div>
 	<?php } ?>
-	
+
 	<?php if ((getNumImages()) > 0) { ?>
-	
+
 	<h4 class="blockhead">
 		<?php if ($useGslideshow) { ?>
 			<div class="slideshowlink"><?php printSlideShowLink(gettext('Slideshow')); ?></div>
 		<?php } elseif ($zpfocus_use_colorbox_slideshow) { ?>
-			<?php 
-			$x=0; 
-			while (next_image(true)): 
-				if ($x>=1) { 
-					$show='style="display:none;"'; 
-				} else { 
-					$show=''
-				} 
+			<?php
+			$x=0;
+			while (next_image()):
+				if ($x>=1) {
+					$show='style="display:none;"';
+				} else {
+					$show='';
+				}
 			?>
 		<?php if (!isImageVideo()) { ?>
 				<a class="slideshowlink"<?php echo $show; ?> rel="slideshow" href="<?php if ($zpfocus_cbtarget) { echo htmlspecialchars(getDefaultSizedImage()); } else { echo htmlspecialchars(getUnprotectedImageURL()); } ?>" title="<?php echo getBareImageTitle();?>"><?php echo gettext('Play Slideshow'); ?></a>
@@ -71,7 +71,7 @@
 		<?php } ?>
 		<span><?php echo gettext('Images in '); echo getBareAlbumTitle(); ?> (<?php echo getNumImages(); ?>)</span>
 	</h4>
-	
+
 	<div class="image-wrap">
 		<ul>
 			<?php while (next_image()): ?>
@@ -85,7 +85,7 @@
 				</div>
 				<a class="thumb" href="<?php echo htmlspecialchars(getImageURL());?>" title="<?php echo getBareImageTitle();?>">
 					<?php printCustomSizedImage(getBareImageTitle(),null,160,120,160,120,null,null,'thumb',null,true); ?>
-				</a>						
+				</a>
 			<?php } else { ?>
 			<li class="thumb-portrait">
 				<div class="album-tools-portrait">
@@ -95,7 +95,7 @@
 						<?php } ?>
 				</div>
 				<a class="thumb" href="<?php echo htmlspecialchars(getImageURL());?>" title="<?php echo getBareImageTitle();?>">
-					<?php printCustomSizedImage(getBareImageTitle(),null,120,160,120,160,null,null,'thumb',null,true); ?>					
+					<?php printCustomSizedImage(getBareImageTitle(),null,120,160,120,160,null,null,'thumb',null,true); ?>
 				</a>
 			<?php } ?>
 			</li>
@@ -103,12 +103,12 @@
 		</ul>
 	</div>
 	<?php } ?>
-	
-	<?php if ( (getPrevPageURL()) || (getNextPageURL()) ) { 
+
+	<?php if ( (getPrevPageURL()) || (getNextPageURL()) ) {
 			printPageListWithNav('« '.gettext('Prev'), gettext('Next').' »',false,'true','page-nav','',true,'5' ); ?>
-	 } 
+	 }
 	 ?>
-	
+
 	<?php if (function_exists('printGoogleMap')) { ?>
 			<div class="gmap">
 		<?php setOption('gmap_width',600,false); printGoogleMap(); ?>
@@ -116,7 +116,7 @@
 	<?php } ?>
 	<?php printCodeblock(); ?>
 	<?php if (function_exists('printCommentForm')) printCommentForm(); ?>
-	
+
 </div>
 
 <?php include("inc-footer.php"); ?>
