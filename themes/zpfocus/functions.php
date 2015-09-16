d084178b9804e65fa6b8dfba39291e8e0b5f253f
fretzl <f.sondaar@kpnmail.nl>
2015-09-16 17:21:41 +0200

    Revert my_checkPageValidity()

--- themes/zpfocus/functions.php
+++ themes/zpfocus/functions.php
@@ -206,41 +206,4 @@ function printLatestNewsCustom($number=5, $category = '', $showdate = true, $sho
 	}
 	echo "</div>\n";
 }
-
-function my_checkPageValidity($request, $gallery_page, $page) {
-	switch ($gallery_page) {
-		case 'gallery.php':
-			$gallery_page = 'index.php'; //	same as an album gallery index
-			break;
-		case 'index.php':
-			if (extensionEnabled('zenpage')) {
-				if (getOption('zenpage_zp_index_news')) {
-					$gallery_page = 'news.php'; //	really a news page
-					break;
-				}
-				if (getOption('zenpage_homepage')) {
-					return $page == 1; // only one page if zenpage enabled.
-				}
-			}
-			break;
-		case 'news.php':
-		case 'album.php':
-		case 'search.php':
-			break;
-		default:
-			if ($page != 1) {
-				return false;
-			}
-	}
-	return checkPageValidity($request, $gallery_page, $page);
-}
-
-if (!OFFSET_PATH) {
-	enableExtension('print_album_menu', 1 | THEME_PLUGIN, false);
-	setOption('user_logout_login_form', 2, false);
-	$_zp_page_check = 'my_checkPageValidity';
-	if (extensionEnabled('zenpage') && getOption('zenpage_zp_index_news')) { // only one index page if zenpage plugin is enabled & displaying
-		zp_register_filter('getLink', 'newsOnIndex');
-	}
-}
 ?>
