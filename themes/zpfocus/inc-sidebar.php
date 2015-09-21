4fb5097ae1306fec04b4c7c853ef83720228cd7a
fretzl <f.sondaar@kpnmail.nl>
2015-09-21 20:25:23 +0200

    zpFocus fix deprecated function

--- themes/zpfocus/inc-sidebar.php
+++ themes/zpfocus/inc-sidebar.php
@@ -1,20 +1,20 @@
 <div class="left">
-	
+
 	<?php
 	if (($zpfocus_showrandom) != 'none') {
 			if (($zpfocus_showrandom) == 'single') {
-					 $sscount=1; 
-			} else { 
-					$sscount=5; 
+					 $sscount=1;
+			} else {
+					$sscount=5;
 			}
-	if ((in_context(ZP_ALBUM)) || (in_context(ZP_IMAGE))) { 
-			$sstype = 'album'; 
-			$ssalbum = $_zp_current_album->getFolder(); 
-			$sstitle = gettext('Random Album Image'); 
-	} else { 
-			$sstype = 'all'; 
-			$ssalbum = null; 
-			$sstitle = gettext('Random Gallery Image'); 
+	if ((in_context(ZP_ALBUM)) || (in_context(ZP_IMAGE))) {
+			$sstype = 'album';
+			$ssalbum = $_zp_current_album->getFilename();
+			$sstitle = gettext('Random Album Image');
+	} else {
+			$sstype = 'all';
+			$ssalbum = null;
+			$sstitle = gettext('Random Gallery Image');
 	}
 	?>
 	<div id="random-wrap">
@@ -24,27 +24,27 @@
 			</div>
 	</div>
 	<?php } ?>
-	
+
 	<!-- PRINTS ZENPAGE EXTRA CONTENT FOR NEW ITEMS, IF APPLICABLE -->
 	<?php if (($zenpage) && (getNewsExtraContent()) ) { ?>
 		<div class="extracontent">
 			<?php printNewsExtraContent(); ?>
 		</div>
 	<?php } ?>
-	
+
 	<!-- PRINTS ZENPAGE EXTRA CONTENT FOR PAGE ITEMS, IF APPLICABLE -->
 	<?php if (($zenpage) && (getPageExtraContent()) ) { ?>
 		<div class="extracontent">
 			<?php printPageExtraContent(); ?>
 		</div>
 	<?php } ?>
-	
+
 	<!-- PRINTS GALLERY DESCRIPTION -->
 	<h4 class="blockhead"><span><?php echo gettext('About'); ?></span></h4>
 		<?php printGalleryDesc( ); ?>
-	
+
 	<!-- PRINTS ZENPAGE LATEST NEWS, IF APPLICABLE -->
-	<?php if (($zenpage) && (getLatestNews()) &&  ($_zp_gallery_page != 'news.php') && ($zpfocus_news) ) { 
+	<?php if (($zenpage) && (getLatestNews()) &&  ($_zp_gallery_page != 'news.php') && ($zpfocus_news) ) {
 		if ((($zpfocus_spotlight) == 'latest') && ($_zp_gallery_page == 'index.php')) { ?>
 			<div id="spotlight2">
 				<h4 class="blockhead"><span><?php echo gettext('More News'); ?></span></h4>
@@ -55,17 +55,17 @@
 		<?php printLatestNewsCustom( 3, '', true, true, 150, true, null ); ?>
 	<?php } ?>
 	</div>
-	<?php } ?>			
-	
+	<?php } ?>
+
 	<!-- PRINTS COMMENTS IN SIDEBAR -->
-	
+
 	<!-- PRINTS LATEST DISQUS COMMENTS, IF PLUGIN EXISTS -->
 	<?php if (function_exists('printLatestDisqus')) { ?>
 		<h4 class="blockhead"><span><?php echo gettext('Latest Comments'); ?></span></h4>
 		<?php printLatestDisqus(3,100);
-	} else { 
+	} else {
 	?>
-	
+
 	<!-- IF NO DISQUS, PRINTS LATEST GALLERY COMMENTS OR NEWS COMMENTS -->
 	<?php if ( $_zp_gallery_page != 'news.php') { ?>
 		<?php if ((function_exists('printCommentForm')) && (getLatestComments(1,'all')) ) { ?>
@@ -77,11 +77,11 @@
 			<h4 class="blockhead"><span><?php echo gettext('Latest News Comments'); ?></span></h4>
 		<?php printLatestComments(3,'100'); ?>
 	<?php } ?>
-<?php } 
-	
-} 
+<?php }
+
+}
 ?>
-	
+
 	<!-- PRINTS NEWS CATEGORIES IF IN NEWS CONTEXT -->
 	<?php if ( $_zp_gallery_page == 'news.php') { ?>
 			<div id="newscats">
