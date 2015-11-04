7b4dcb8190416aba6a83d9597e2beb7c8c40a50b
fretzl <f.sondaar@kpnmail.nl>
2015-11-04 11:25:45 +0100

    Fix menu Gallery URL

--- themes/zpskeleton/themeoptions.php
+++ themes/zpskeleton/themeoptions.php
@@ -15,14 +15,14 @@ class ThemeOptions {
 		setThemeOption('thumb_size',220,null,'zpskeleton');
 		setThemeOption('image_size',630,null,'zpskeleton');
 		setThemeOption('image_use_side','longest',null,'zpskeleton');
-		setThemeOption('custom_index_page','none',null,'zpskeleton');
-		
+		setThemeOption('custom_index_page',null,null,'zpskeleton');
+
 		// set core theme option defaults
 		setThemeOptionDefault('albums_per_page',6);
-		setThemeOptionDefault('images_per_page',9); 
+		setThemeOptionDefault('images_per_page',9);
 		setThemeOptionDefault('thumb_transition',2);
 		setThemeOptionDefault('thumb_crop',0);
-		
+
 		// set theme option defaults
 		setThemeOptionDefault('zpskel_usenews', true);
 		setThemeOptionDefault('zpskel_debuguser', false);
@@ -43,50 +43,50 @@ class ThemeOptions {
 			cacheManager::addThemeCacheSize('zpskeleton',630,null,null,null,null,null,null,false,getOption('fullimage_watermark'),null,null); // desktop full
 		}
 	}
-	
+
 	function getOptionsSupported() {
-		return array(	
-			gettext('Use News Feature') => array('key' => 'zpskel_usenews', 'type' => OPTION_TYPE_CHECKBOX, 
+		return array(
+			gettext('Use News Feature') => array('key' => 'zpskel_usenews', 'type' => OPTION_TYPE_CHECKBOX,
 				'order' => 5,
 				'desc' => gettext("IF you have the Zenpage plugin enabled, you can uncheck this to NOT use the news feature of the Zenpage plugin (use only pages)")),
 			gettext('Thumb Size') => array('key' => 'zpskel_thumbsize', 'type' => OPTION_TYPE_RADIO,
 				'order' => 4,
 				'buttons' => array(gettext('Large')=>'large', gettext('Small')=>'small'),
-				'desc' => gettext('Choose the relative thumb size for the album and search pages.')),							
-			gettext('Debug User Agent') => array('key' => 'zpskel_debuguser', 'type' => OPTION_TYPE_CHECKBOX, 
+				'desc' => gettext('Choose the relative thumb size for the album and search pages.')),
+			gettext('Debug User Agent') => array('key' => 'zpskel_debuguser', 'type' => OPTION_TYPE_CHECKBOX,
 				'order' => 1,
 				'desc' => gettext("Check to display user information in the footer for debugging mobile displays.")),
-			gettext('Disable Plugin Warning') => array('key' => 'zpskel_disablewarning', 'type' => OPTION_TYPE_CHECKBOX, 
+			gettext('Disable Plugin Warning') => array('key' => 'zpskel_disablewarning', 'type' => OPTION_TYPE_CHECKBOX,
 				'order' => 0,
 				'desc' => gettext("Check to disable the warning message about disabling plugins on the index page.  If you see no warning, you should also check this as well to eliminate the plugin verifications.")),
-			gettext('Popup (PrettyPhoto) Target') => array('key' => 'zpskel_pptarget', 'type' => OPTION_TYPE_RADIO, 
+			gettext('Popup (PrettyPhoto) Target') => array('key' => 'zpskel_pptarget', 'type' => OPTION_TYPE_RADIO,
 				'order' => 2,
 				'buttons' => array(gettext('Sized (630px)')=>'sized', gettext('Original Image')=>'original'),
 				'desc' => gettext("Select whether the popup script (PrettyPhoto) targets a processed/sized image for the slideshow or the original (unsized/uprocessed) uploaded image.  Although not used on mobile decives, this will decrease your load time if you select sized.  You need to also select sized if you are using watermarks.")),
-			gettext('Download Button') => array('key' => 'zpskel_download', 'type' => OPTION_TYPE_CHECKBOX, 
+			gettext('Download Button') => array('key' => 'zpskel_download', 'type' => OPTION_TYPE_CHECKBOX,
 				'order' => 6,
 				'desc' => gettext("Check to display a download button on the image page, behavour controlled by options->image->Full Image Protection.")),
-			gettext('Show Archive') => array('key' => 'zpskel_archive', 'type' => OPTION_TYPE_CHECKBOX, 
+			gettext('Show Archive') => array('key' => 'zpskel_archive', 'type' => OPTION_TYPE_CHECKBOX,
 				'order' => 7,
 				'desc' => gettext("Check to display the gallery and news archive menu item.")),
-			gettext('Homepage') => array('key' => 'zenpage_homepage', 'type' => OPTION_TYPE_CUSTOM, 
+			gettext('Homepage') => array('key' => 'zenpage_homepage', 'type' => OPTION_TYPE_CUSTOM,
 				'order' => 8,
 				'desc' => gettext("Choose here any <em>un-published Zenpage page</em> (listed by <em>titlelink</em>) to act as your site's homepage instead the normal gallery index.")),
-									
-			gettext('Image Strip') => array('key' => 'zpskel_strip', 'type' => OPTION_TYPE_RADIO, 
+
+			gettext('Image Strip') => array('key' => 'zpskel_strip', 'type' => OPTION_TYPE_RADIO,
 				'order' => 3,
 				'buttons' => array(gettext('Latest Images')=>'latest', gettext('Random Images')=>'random'),
 				'desc' => gettext("Select whether the image strip on the bottom shows random images or latest images.")),
-			gettext('Custom CSS') => array('order'=>9, 'key' => 'zpskel_customcss', 'type' => OPTION_TYPE_TEXTAREA, 'desc' => gettext('Enter any custom CSS, safely carries over upon theme upgrade.'))					
-			
-		
+			gettext('Custom CSS') => array('order'=>9, 'key' => 'zpskel_customcss', 'type' => OPTION_TYPE_TEXTAREA, 'desc' => gettext('Enter any custom CSS, safely carries over upon theme upgrade.'))
+
+
 		);
 	}
 
 	function getOptionsDisabled() {
 		return array('thumb_size','image_size','image_use_side1','image_use_side2','image_use_side3','image_use_side4','custom_index_page');
 	}
-	
+
 	function handleOption($option, $currentValue) {
 		if($option == "zenpage_homepage") {
 			$unpublishedpages = query_full_array("SELECT titlelink FROM ".prefix('pages')." WHERE `show` != 1 ORDER by `sort_order`");
